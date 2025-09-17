c0180a059fde05743f068b12a12d9648477abc59
Jai Soni <jaiusoni2003@gmail.com>
2025-09-17 22:46:05 +0530

    update server.js

--- src/server.js
+++ src/server.js
@@ -20,7 +20,11 @@ const io = new Server(server, {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: env.FRONTEND_URL,
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Rate Limiting
